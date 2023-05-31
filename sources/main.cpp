f3fac0a8feb674d1f06971d5234622c24d5cf32e
BuildTools <unconfigured@null.spigotmc.org>
2023-05-31 18:58:53 -0500

    why did I leave a space there??

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,5 +1,8 @@
+#include <vector>
+#include <string_view>
 
-int main(int argc,char * argv[]) {
-	
+int main(int argc,char* argv[]) {
+	const std::vector<std::string_view> args(argv,argv+argc);
+
 	return 0;
 }

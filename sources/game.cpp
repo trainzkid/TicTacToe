a6ecf45e31159b17e5dc3ec3e780bb60947aaa32
trainzkid <trainzkid@gmail.com>
2023-06-15 02:32:39 -0500

    Corrected the check that a move is allowed

--- sources/game.cpp
+++ sources/game.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <iostream>
 #include <utility>
+#include <limits>
 
 namespace TicTacToe {
 	Game::Game(long unsigned int boardSize) : board{boardSize,std::vector<char>(boardSize,'_')},players{'x','o'},isRunning{true} {
@@ -30,17 +31,28 @@ namespace TicTacToe {
 		while(isRunning) {
 			// ask players one at a time where they'd like to place their mark
 			for(const char player : players) {
+				std::cout<<"Player "<<player<<", please choose a space to place your mark. "<<std::endl;
 				std::pair<int,int> coordinate{};
-				 do {
+
+				bool moveAllowed = false;
+				 while(!moveAllowed) {
 					std::cout<<"Column: ";
 					std::cin>>coordinate.first;
+					// std::cin.ignore(std::numeric_limits<std::streamsize>::max()); causes stall
 					std::cout<<"Row: ";
 					std::cin>>coordinate.second;
+					// std::cin.ignore(std::numeric_limits<std::streamsize>::max()); causes stall
 
 					// correct for 0-based index
 					coordinate.first--;
 					coordinate.second--;
-				} while(board[coordinate.second][coordinate.first]!='_');	// check if requested place is valid (doesn't contain another player's mark)
+
+					// check if requested place is valid (doesn't contain another player's mark)
+					if(board[coordinate.second][coordinate.first]=='_')
+						moveAllowed = true;
+					else
+						std::cout<<"That space has already been taken! Pick another. "<<std::endl;
+				} 	
 
 				board[coordinate.second][coordinate.first]=player;
 
